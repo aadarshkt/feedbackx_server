6fedaac91fff00aed8775fffd2ada7be18ca42f8
aadarshkt <aadarshkt1729@gmail.com>
2024-02-04 12:11:55 +0530

    connected to database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -14,6 +15,25 @@ app.use(
 
 dotenv.config();
 const port = process.env.PORT || 8080;
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+
+//connect to mongoDB
+const uri = `mongodb+srv://${username}:${password}@feedbackcluster.rimgkgl.mongodb.net/?retryWrites=true&w=majority`;
+const clientOptions = { serverApi: { version: "1", strict: true, deprecationErrors: true } };
+async function run() {
+  try {
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.log(error);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run();
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Success" });
